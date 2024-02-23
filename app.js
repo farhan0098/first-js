cd216ac45857c4e236845a3333db1ede9f91f85b
farhan0098 <159815493+farhan0098@users.noreply.github.com>
2024-02-23 23:04:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var number1= 200;
+var number2= 150;
+
+var addmessage ="hrer is your sum"
+var submessage ="here is your sub"
+var mulmessage ="here is your mul"
+var divmessage ="here is your division"
+var permessage ="your percetage"
+alert (permessage + number1/number2 * 100)
